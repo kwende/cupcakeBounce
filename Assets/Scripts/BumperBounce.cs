d0917e8cea852ec0a90c23ec5e5aa1633703363b
kwende <ben@ben-rush.net>
2015-07-07 23:50:19 -0500

    first commit

---
+++ Assets/Scripts/BumperBounce.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BumperBounce : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D collisionObject)
+	{
+		Rigidbody2D rigidBody = collisionObject.GetComponent<Rigidbody2D> (); 
+
+		Vector2 offset = collisionObject.transform.position - transform.position; 
+
+		rigidBody.velocity = offset.normalized * rigidBody.velocity.magnitude * 1.1f;  
+	}
+}
