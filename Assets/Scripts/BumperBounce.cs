c30bd67f6897a3ff027a6649158ae63afb4aaddd
kwende <ben@ben-rush.net>
2015-07-09 15:16:26 -0500

    Code cleanup

--- Assets/Scripts/BumperBounce.cs
+++ Assets/Scripts/BumperBounce.cs
@@ -30,18 +30,8 @@ public class BumperBounce : MonoBehaviour {
 		}
 
 		Vector2 newVelocity = (collisionObject.transform.position - transform.position).normalized * 15f; 
-
-		Vector2 flippedOriginalVelocity = rigidBody.velocity.normalized * -1.0f; 
-		Vector2 newVelocityNormalized = newVelocity.normalized; 
-
-		float dotProduct = Vector2.Dot (flippedOriginalVelocity, newVelocityNormalized); 
-
-		RaycastHit2D hit = Physics2D.Raycast (rigidBody.transform.position, rigidBody.velocity); 
-
-		Vector2 hitPoint = hit.point; 
 		Vector2 centerOfObject = rigidBody.centerOfMass; 
 
-		//rigidBody.angularVelocity = dotProduct * rigidBody.velocity.magnitude * -20 * Mathf.Sign(newVelocity.x); 
 		rigidBody.AddForceAtPosition(-newVelocity * 2, centerOfObject); 
 		rigidBody.velocity = newVelocity; 
 	}
