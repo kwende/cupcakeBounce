a25cbb708f791f1785c01b0832c4680cb29bf604
Ben Rush <brush@nebraskaglobal.com>
2015-07-09 12:40:12 -0500

    got bouncing working.

--- Assets/Scripts/BumperBounce.cs
+++ Assets/Scripts/BumperBounce.cs
@@ -9,6 +9,20 @@ public class BumperBounce : MonoBehaviour {
 
 		Vector2 offset = collisionObject.transform.position - transform.position; 
 
-		rigidBody.velocity = offset.normalized * rigidBody.velocity.magnitude * 1.1f;  
+		Vector2 newVelocity = offset.normalized * 15f; //rigidBody.velocity.magnitude * 1.1f;
+		Vector2 originalVelocity = rigidBody.velocity.normalized; 
+
+		Vector2 flippedOriginalVelocity = originalVelocity * -1.0f; 
+		Vector2 newVelocityNormalized = newVelocity.normalized; 
+
+		float dotProduct = Vector2.Dot (flippedOriginalVelocity, newVelocityNormalized); 
+
+		float angleInRadians = Mathf.Acos (dotProduct); 
+
+		float angularVelocityInDegrees = angleInRadians * 180.0f / Mathf.PI;  
+
+		rigidBody.angularVelocity = 2 * angularVelocityInDegrees * -Mathf.Sign(newVelocity.x); 
+
+		rigidBody.velocity = newVelocity; 
 	}
 }
