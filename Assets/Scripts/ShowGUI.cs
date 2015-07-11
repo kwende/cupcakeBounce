56a5a73517094320992c79f2506716863c7b3214
kwende <ben@ben-rush.net>
2015-07-11 01:35:27 -0500

    on a key down, will show a panel now.

---
+++ Assets/Scripts/ShowGUI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowGUI : MonoBehaviour {
+
+	public GameObject UIPrefab; 
+	private GameObject _windowInstance; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.T) && _windowInstance == null) {
+			_windowInstance = Instantiate(UIPrefab); 
+			GameObject go = GameObject.Find("TheCanvas"); 
+			_windowInstance.transform.SetParent(go.transform, false); 
+		} 
+		else if (Input.GetKeyDown(KeyCode.T) && _windowInstance != null){
+			Destroy(_windowInstance); 
+		}
+	}
+}
