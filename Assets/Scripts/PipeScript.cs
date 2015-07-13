43e7026f16c1dc9eb5d561f29a117d7fd86f2c1e
Ben Rush <ben@ben-rush.net>
2015-07-13 10:05:36 -0500

    Pipes and bricks.

---
+++ Assets/Scripts/PipeScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PipeScript : MonoBehaviour 
+{
+	void Start(){
+		this.gameObject.tag = "pipe"; 
+	}
+
+	void OnTriggerEnter2D(Collider2D collisionObject)
+	{
+		Destroy (collisionObject.gameObject); 
+
+		GameObject[] allPipes = GameObject.FindGameObjectsWithTag ("pipe"); 
+	}
+}
