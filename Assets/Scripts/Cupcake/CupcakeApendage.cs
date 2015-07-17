ecc6820aa37c2a3795b44cae6b3055dc1fb10e61
Ben Rush <ben@ben-rush.net>
2015-07-17 00:57:42 -0500

    individual pieces of the bear can be blown off now.

---
+++ Assets/Scripts/Cupcake/CupcakeApendage.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CupcakeApendage : MonoBehaviour {
+
+	void Start(){
+	}
+
+	void OnCollisionEnter2D(Collision2D collidedWith){
+		if (collidedWith.rigidbody != null && 
+		    collidedWith.rigidbody.name.Contains("cannonball") && 
+		    collidedWith.rigidbody.velocity.magnitude > 10) {
+			// splinter. 
+			DistanceJoint2D joint = this.gameObject.GetComponent<DistanceJoint2D> (); 
+			joint.enabled = false;
+		}
+	}
+}
